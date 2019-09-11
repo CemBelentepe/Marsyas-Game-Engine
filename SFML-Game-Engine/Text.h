853efb769821000ff2ee9402de61f1b69518ed28
Cem Belentepe <cemcembelentepe@gmail.com>
2019-09-11 15:27:44 +0300

    Few changes

--- SFML-Game-Engine/Text.h
+++ SFML-Game-Engine/Text.h
@@ -6,8 +6,16 @@ namespace mge
 {
 	class Text : public UIComponent
 	{
+	private:
 	public:
 		sf::Text sfText;
+		enum AlignMode
+		{
+			TOP_LEFT, TOP_CENTER, TOP_RIGHT,
+			CENTER_LEFT, CENTER, CENTER_RIGHT,
+			BOTTOM_LEFT, BOTTOM_CENTER, BOTTOM_RIGHT
+		};
+
 		sf::Font font;
 
 		Text(GameObject* gameObject) :UIComponent(gameObject)
@@ -25,5 +33,13 @@ namespace mge
 
 		void update();
 		void render(sf::RenderWindow* window);
+
+		void setText(sf::String text);
+		void setAlign(AlignMode align);
+		void setFontSize(unsigned int fontSize);
+
+		sf::String getText();
+		AlignMode getAlign();
+		unsigned int getFontSize();
 	};
 }
